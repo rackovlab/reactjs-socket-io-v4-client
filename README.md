98c05136b16a20e0dc060b82e85192c473adeb08
Nugroho Ardi Sutrisno <nugroho@prospace.io>
2025-02-09 15:37:05 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# reactjs-socket-io-v4-client
+# React.js Socket.IO v4 Client
+
+This is a simple React.js client that connects to a Socket.IO v4 server using WebSockets. Find the Go/Golang server code here `https://github.com/rackovlab/go-socket-server`
+
+## Features
+
+- Establishes a connection to a Socket.IO server
+- Utilize socket.io-client v4.8.1
+- Sends messages (both string and JSON object) to the server
+
+## Installation
+
+1. Clone the repository
+
+   ```sh
+   git clone https://github.com/rackovlab/reactjs-socket-io-v4-client.git
+   cd your-repo-name
+   ```
+
+2. Install dependencie
+
+   ```sh
+   npm install
+   ```
+
+3. Start the React application
+   ```sh
+   npm start
+   ```
+
+## Usage
+
+- Ensure the Socket.IO server is running at ws://localhost:3300 (or update the SERVER_URL in App.js to match your server address).
+- The application connects to the server on load and listens for messages on the test event.
+- Use the buttons to emit messages to the server.
---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const SERVER_URL = "ws://localhost:3300"; // Replace with your server URL
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [mSocket, setMSocket] = useState();
+
+  // Establish connection to the Socket.IO server
+  useEffect(() => {
+    const socket = io(SERVER_URL, {
+      transports: ["websocket"], // Ensures WebSocket is used for communication
+      withCredentials: true, // Allows sending credentials (cookies, authentication headers, etc.)
+    });
+
+    // Event listener for successful connection
+    socket.on("connect", () => {
+      console.log("Connection success");
+      setMSocket(socket); // Save socket instance to state for further use
+    });
+
+    // Event listener for connection errors
+    socket.on("connect_error", (err) => {
+      console.error("Connection error:", err);
+    });
+
+    // Listen for "test" events from the server
+    socket.on("test", (data) => {
+      setMessage(data); // Update state with received message
+      console.log("Received msg:", data);
+    });
+
+    // Cleanup function to disconnect from the server when component unmounts
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  // Function to emit a string message to the server on the "test" event
+  const emitString = () => {
+    mSocket.emit("test", "hello world");
+  };
+
+  // Function to emit a JSON-stringified object to the server on the "test" event
+  const emitObj = () => {
+    const chatMessage = { foo: "bar" };
+    mSocket.emit("test", JSON.stringify(chatMessage));
+  };
+
+  return (
+    <div>
+      <h1>Socket.IO React Client</h1>
+      <p>Message from server: {message}</p>
+      <div>
+        <p>
+          Click to emit string :{" "}
+          <button onClick={emitString}>Emit string</button>
+        </p>
+        <p>
+          Click to emit stringified js object :{" "}
+          <button onClick={emitObj}>Emit obj</button>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
